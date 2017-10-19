f8a5913a76d45be1ede49bab8dbbe4aa73d38aaf
Ngoc Tran <ngoctnq@me.com>
2017-10-19 00:56:48 -0400

    temp

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -20,9 +20,9 @@ public:
 		double t[dim-1][dim-1];
 		double dtmnt=0;
 		if (dim==1)
-			return matrix[0][0];
+			return m[0][0];
 		else if  (dim==2)
-			return matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0];
+			return m[0][0]*m[1][1]-m[0][1]*m[1][0];
 		else {
 			for( int i=0; i<dim;i++) {
 				for(int j =1;j<dim;j++) {
@@ -30,14 +30,14 @@ public:
 					for (int k=0; k<dim ;k++) {
 						if (k==i) 
 							continue;
-						t[j-1][j1]=matrix[j][k];
+						t[j-1][j1]=m[j][k];
 						j1++; 
 					}
 				}
 				double c = 1;
 				if ((i%2)==1)
 					c=-1;
-				dtmnt= dtmnt + c*matrix[0][i]*this->dtmnt(dim-1, t);
+				dtmnt= dtmnt + c*m[0][i]*this->dtmnt(dim-1, t);
 			}
 			return dtmnt;
 		}
