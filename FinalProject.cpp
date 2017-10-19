28983e70766ad7bf9046c75d5e88768180d5ee9d
Ngoc Tran <ngoctnq@me.com>
2017-10-19 00:53:36 -0400

    temp

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -16,29 +16,30 @@ public:
 	double element(int i, int j) {return matrix[i][j];}
 	void change(int i, int j,double newValue) {matrix[i][j] =newValue;}
 	
-	double dtmnt(int dim, Matrix matrix) {
-		double t[99][99];
+	double dtmnt(int dim, double **m) {
+		double t[dim-1][dim-1];
 		double dtmnt=0;
 		if (dim==1)
-			return m[0][0];
+			return matrix[0][0];
 		else if  (dim==2)
-			return mtx[0][0]*mtx[1][1]-mtx[0][1]*mtx[1][0];
+			return matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0];
 		else {
 			for( int i=0; i<dim;i++) {
 				for(int j =1;j<dim;j++) {
 					int j1=0;
-					for (int k=0; k<n;k++) {
-						if (k==c) 
+					for (int k=0; k<dim ;k++) {
+						if (k==i) 
 							continue;
-						t[j-1][j1]=m[j][k];
+						t[j-1][j1]=matrix[j][k];
 						j1++; 
 					}
 				}
-			double c = 1;
-			if ((i%2)==1)
-				c=-1;
-			dtmnt= dtmnt + c*m[0][i]*dtmnt(dim-1,t);
+				double c = 1;
+				if ((i%2)==1)
+					c=-1;
+				dtmnt= dtmnt + c*matrix[0][i]*this->dtmnt(dim-1, t);
 			}
+			return dtmnt;
 		}
 	}
 	
@@ -55,9 +56,10 @@ int main()
 	double m[n][n];
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++)
-			cin>m[i][j];
+			cin >> m[i][j];
 	}
-	Matrix matrix(m[n][n]);
-	return matrix.dtmnt;
+	Matrix matrix = new Matrix(m);
+	cout << matrix.dtmnt(n, m);
+	return 0;
 }
 		
