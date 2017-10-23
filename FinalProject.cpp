caeff84adfdc1afb80ef24a531174b3d349b6411
Ngoc Tran <9092752+ngoctnq@users.noreply.github.com>
2017-10-23 14:56:26 -0400

    Update FinalProject.cpp

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -1,65 +1,57 @@
-/*File: FinalProject.cpp
-Author: Wayne Nguyen
-*/
-
 #include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <numeric>
+#include <memory>
 
-using std::cout;
-using std::cin;
-using std::endl;
+using namespace std;
+double dtmnt(int dim, vector<vector <double>> matrix) {
+                vector< vector<double> > t;
+                double d=0;
+                if (dim==1)
+                        return matrix[0][0];
+                else if  (dim==2)
+                        return matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0];
+                else {
+                        for( int i=0;i<dim;i++) {
+                                for(int j =1;j<dim;j++) {
+                                        vector<double> row;
+                                        for (int k=0; k<dim ;k++) {
+                                                if (k==i)
+                                                        continue;
+                                                row.push_back(matrix[j][k]);
+                                        }
+                                        t.push_back(row);
+                                }
+                       }
+                	if ((i%2)==0)
+                	d  += matrix[0][i]*dtmnt(dim-1, t);
+                	if ((i%2)==1)
+                	d -= matrix[0][i]*dtmnt(dim-1, t);
+                }
+	        return d;
+        }
 
-
-class Matrix {
-public:
-	Matrix(double **m) {matrix = m;}
-
-	double element(int i, int j) {return matrix[i][j];}
-	void change(int i, int j,double newValue) {matrix[i][j] =newValue;}
-	
-	double dtmnt(int dim, double **m) {
-		double t[dim-1][dim-1];
-		double dtmnt=0;
-		if (dim==1)
-			return m[0][0];
-		else if  (dim==2)
-			return m[0][0]*m[1][1]-m[0][1]*m[1][0];
-		else {
-			for( int i=0; i<dim;i++) {
-				for(int j =1;j<dim;j++) {
-					int j1=0;
-					for (int k=0; k<dim ;k++) {
-						if (k==i) 
-							continue;
-						t[j-1][j1]=m[j][k];
-						j1++; 
-					}
-				}
-				double c = 1;
-				if ((i%2)==1)
-					c=-1;
-				dtmnt= dtmnt + c*m[0][i]*this->dtmnt(dim-1, t);
-			}
-			return dtmnt;
-		}
-	}
-	
-private:
-	int dim;
-	double **matrix; 
-};
 
 int main()
 {
-	cout<<"Enter the dimension of the square matrix: " <<endl;
+	cout <<"Enter dimension: " <<endl;
 	int n;
-	cin >> n;
-	double m[n][n];
+	cin>>n;
+	vector < vector<double> > m;
 	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++)
-			cin >> m[i][j];
+		vector<double> row;
+		for(int j=0;j<n;j++){
+			double k;
+			cin >>k;
+			row.push_back(k);
+		}
+		m.push_back(row);	
 	}
-	Matrix matrix = new Matrix(m);
-	cout << matrix.dtmnt(n, m);
-	return 0;
+	cout << dtmnt(n,m)<<endl;
 }
-		
+	
