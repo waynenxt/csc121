d9d7fbd9fbff8964e8436d5090c8d488f37028ab
waynenxt <32919549+waynenxt@users.noreply.github.com>
2017-10-23 15:23:45 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,32 +9,35 @@
 #include <memory>
 
 using namespace std;
+
+//Function used to calculate determinant
 double dtmnt(int dim, vector<vector <double>> matrix) {
-                vector< vector<double> > t;
-                double d=0;
-                if (dim==1)
+                double d=0;     								 //determinant
+                if (dim==1)									 //If dimension = 1
                         return matrix[0][0];
-                else if  (dim==2)
+                else if  (dim==2)								 //If dimension = 2
                         return matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0];
-                else {
+                else { 										 //dimension >=3 
                         for( int i=0;i<dim;i++) {
-                                for(int j =1;j<dim;j++) {
-                                        vector<double> row;
+                                        vector< vector<double> > t; 				 //Submatrix
+				        for(int j =1;j<dim;j++) {
+                                        vector<double> row;      
                                         for (int k=0; k<dim ;k++) {
                                                 if (k==i)
                                                         continue;
                                                 row.push_back(matrix[j][k]);
                                         }
-                                        t.push_back(row);
+                                        t.push_back(row);					 //Input submatrix
                                 }
-                                if ((i%2)==0)
-                                d  += matrix[0][i]*dtmnt(dim-1, t);
-                                if ((i%2)==1)
+                           if ((i%2)==0)							 //Calculate determinant
+                           d  += matrix[0][i]*dtmnt(dim-1, t);
+                           if ((i%2)==1)
                                  d -= matrix[0][i]*dtmnt(dim-1, t);
 
                        }
-                        return d;
                 }
+                        return d;								 //Output determinant
+
         }
 
 
@@ -53,6 +56,27 @@ int main()
 		}
 		m.push_back(row);	
 	}
-	cout << dtmnt(n,m)<<endl;
+//	double d=0;
+/*	vector< vector<double> > t;
+	                for( int i=0;i<(n);i++) {
+                                for(int j =1;j<n;j++) {
+                                        vector<double> row;
+                                        for (int k=0; k<n;k++) {
+                                                if (k==i)
+                                                        continue;
+                                                row.push_back(m[j][k]);
+                                        }
+                                        t.push_back(row);
+                                }
+				cout << dtmnt(n-1,t) <<endl;
+                                if ((i%2)==0)
+                                d  += m[0][i]*dtmnt(n-1, t);
+                                if ((i%2)==1)
+                                 d -= m[0][i]*dtmnt(n-1, t);
+
+                     }
+                      cout<< d<<endl; */
+		cout <<dtmnt(n,m) <<endl;	
+
 }
 	
